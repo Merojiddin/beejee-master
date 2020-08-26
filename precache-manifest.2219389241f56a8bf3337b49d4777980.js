1e67e5c831027d0d1920b84c1b2f1d191b9df27e
Merojiddin <merojiddin_1998@mail.ru>
2020-08-27 00:01:18 +0800

    Updates

---
+++ precache-manifest.2219389241f56a8bf3337b49d4777980.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0edca72e773b9936943a",
+    "url": "/beejee-master/static/js/runtime~main.0edca72e.js"
+  },
+  {
+    "revision": "e2b2f02ecb7056290d89",
+    "url": "/beejee-master/static/js/main.e2b2f02e.chunk.js"
+  },
+  {
+    "revision": "7b8ac7f06ad63b3adca6",
+    "url": "/beejee-master/static/js/1.7b8ac7f0.chunk.js"
+  },
+  {
+    "revision": "e2b2f02ecb7056290d89",
+    "url": "/beejee-master/static/css/main.5348fe68.chunk.css"
+  },
+  {
+    "revision": "7e40b2d544776c47294e2377fb42966e",
+    "url": "/beejee-master/index.html"
+  }
+];
